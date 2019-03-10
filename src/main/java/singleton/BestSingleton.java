bfc2fd79017eff151c7ec44892d4189aedddf67c
panning255 <panning255@jk.cn>
2019-03-10 13:42:16 +0800

    format code

--- src/main/java/singleton/BestSingleton.java
+++ src/main/java/singleton/BestSingleton.java
@@ -1,28 +1,10 @@
 package singleton;
 
-public class BestSingleton {
+public enum BestSingleton {
 
-    // 私有构造函数
-    private BestSingleton() {
+    INSTANCE;
 
-    }
-
-    public static BestSingleton getInstance() {
-        return Singleton.INSTANCE.getInstance();
-    }
-
-    private enum Singleton {
-        INSTANCE;
-
-        private BestSingleton singleton;
-
-        // JVM保证这个方法绝对只调用一次
-        Singleton() {
-            singleton = new BestSingleton();
-        }
-
-        public BestSingleton getInstance() {
-            return singleton;
-        }
+    public BestSingleton getInstance() {
+        return INSTANCE;
     }
 }
--- src/main/java/singleton/lazySingleton/LazySingleton.java
+++ src/main/java/singleton/lazySingleton/LazySingleton.java
@@ -1,21 +1,46 @@
 package singleton.lazySingleton;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Created by marcopan on 2018/9/14.
+ * 有反射攻击和反序列化攻击的问题
  */
 public class LazySingleton {
     private LazySingleton() {
         // 防止通过反射来破坏单例
-        if (LazySingleHolder.singleton != null) {
+        if (LazySingletonHolder.singleton != null) {
             throw new RuntimeException("不允许创建多个实例");
         }
     }
 
     public static final LazySingleton getInstance() {
-        return LazySingleHolder.singleton;
+        return LazySingletonHolder.singleton;
     }
 
-    private static class LazySingleHolder {
+    public static void main(String[] args) {
+        try {
+            //很无聊的情况下，进行破坏
+            Class<?> clazz = LazySingleton.class;
+
+            //通过反射拿到私有的构造方法
+            Constructor c = clazz.getDeclaredConstructor(null);
+            //强制访问，强吻，不愿意也要吻
+            c.setAccessible(true);
+
+            //暴力初始化
+            Object o1 = c.newInstance();
+
+            //调用了两次构造方法，相当于new了两次,犯了原则性问题，
+            Object o2 = c.newInstance();
+
+            System.out.println(o1 == o2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class LazySingletonHolder {
         private static final LazySingleton singleton = new LazySingleton();
     }
 }
