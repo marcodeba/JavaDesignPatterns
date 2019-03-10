e577e068a3a2a4cfc01b9e8e0460a73a42c0b2a8
panning255 <panning255@jk.cn>
2019-03-10 13:52:44 +0800

    add SeriableSingleton

--- src/main/java/singleton/lazySingleton/LazySingleton.java
+++ src/main/java/singleton/lazySingleton/LazySingleton.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Constructor;
 
 /**
  * Created by marcopan on 2018/9/14.
- * 有反射攻击和反序列化攻击的问题
+ * 有反射攻击和序列化攻击的问题
  */
 public class LazySingleton {
     private LazySingleton() {
@@ -22,7 +22,6 @@ public class LazySingleton {
         try {
             //很无聊的情况下，进行破坏
             Class<?> clazz = LazySingleton.class;
-
             //通过反射拿到私有的构造方法
             Constructor c = clazz.getDeclaredConstructor(null);
             //强制访问，强吻，不愿意也要吻
@@ -30,9 +29,8 @@ public class LazySingleton {
 
             //暴力初始化
             Object o1 = c.newInstance();
-
             //调用了两次构造方法，相当于new了两次,犯了原则性问题，
-            Object o2 = c.newInstance();
+            Object o2 = LazySingleton.getInstance();//c.newInstance();
 
             System.out.println(o1 == o2);
         } catch (Exception e) {
