259e2d7c1e41d28d1f9864933ca67eb85397ea5e
panning255 <panning255@jk.cn>
2019-03-11 22:03:14 +0800

    optimize DynamicProxy

--- src/main/java/proxy/dynamicProxy/Client.java
+++ src/main/java/proxy/dynamicProxy/Client.java
@@ -1,41 +1,13 @@
 package proxy.dynamicProxy;
 
-import sun.misc.ProxyGenerator;
-
-import java.io.FileOutputStream;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-
 /**
  * Created by marcopan on 2018/9/8.
  */
 public class Client {
     public static void main(String[] args) {
         IAbstractSubject iSubject = new RealSubject();
-        InvocationHandler handler = new DynamicProxySubject(iSubject);
 
-        /**
-         * Proxy这个类的作用就是用来动态创建一个代理对象的类
-         * Proxy.newProxyInstance返回的是一个代理对象:com.sun.proxy.$Proxy0
-         * 根据handler.getClass().getClassLoader()和iSubject.getClass().
-         　　　　　　getInterfaces()构造Class对象，Class对象的构造函数入参是handler
-         */
-        IAbstractSubject proxy = (IAbstractSubject) Proxy.newProxyInstance(handler.getClass().getClassLoader(),
-                iSubject.getClass().getInterfaces(),
-                handler);
-        //System.out.println(proxy.getClass().getName());
-        byte[] bytes = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{IAbstractSubject.class});
-        try {
-            String filePath = IAbstractSubject.class.getResource("").getPath();
-            System.out.println(filePath);
-            FileOutputStream fos = new FileOutputStream(filePath + "/$Proxy0.class");
-            fos.write(bytes);
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //调用的是super.h.invoke(this, , )，DynamicProxySubject的invoke方法
+        IAbstractSubject proxy = (IAbstractSubject) new DynamicProxySubject().getProxyInstance(iSubject);
         proxy.requestHouse1();
         proxy.requestHouse2();
     }
--- src/main/java/proxy/dynamicProxy/DynamicProxySubject.java
+++ src/main/java/proxy/dynamicProxy/DynamicProxySubject.java
@@ -1,7 +1,11 @@
 package proxy.dynamicProxy;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * Created by marcopan on 2018/9/8.
@@ -10,8 +14,25 @@ import java.lang.reflect.Method;
 public class DynamicProxySubject implements InvocationHandler {
     private IAbstractSubject target = null;
 
-    public DynamicProxySubject(IAbstractSubject target) {
+    public Object getProxyInstance(IAbstractSubject target) {
         this.target = target;
+        Class<?> clazz = target.getClass();
+
+//        byte[] bytes = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{IAbstractSubject.class});
+//        try {
+//            String filePath = IAbstractSubject.class.getResource("").getPath();
+//            FileOutputStream fos = new FileOutputStream(filePath + "/$Proxy0.class");
+//            fos.write(bytes);
+//            fos.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        /**
+         * Proxy这个类的作用就是用来动态创建一个代理对象的类
+         * Proxy.newProxyInstance返回的是一个代理对象:com.sun.proxy.$Proxy0
+         */
+        return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), this);
     }
 
     /**
@@ -20,8 +41,8 @@ public class DynamicProxySubject implements InvocationHandler {
      * args:指代的是调用真实对象某个方法时接受的参数
      */
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println(method);
-        method.invoke(target, args);
-        return null;
+        // 增强消息......
+        return method.invoke(target, args);
+        // 增强消息......
     }
 }
