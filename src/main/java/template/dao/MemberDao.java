7aac74ea6242eef333371a7a82d6558b41104329
panning255 <panning255@jk.cn>
2019-03-17 12:40:50 +0800

    优化代码

--- src/main/java/template/dao/MemberDao.java
+++ src/main/java/template/dao/MemberDao.java
@@ -16,16 +16,13 @@ public class MemberDao extends JdbcTemplate {
 
     public List<?> selectAll() {
         String sql = "select * from t_member";
-        return super.executeQuery(sql, new RowMapper<Member>() {
-            @Override
-            public Member mapRow(ResultSet rs, int rowNum) throws Exception {
-                Member member = new Member();
-                member.setUsername(rs.getString("username"));
-                member.setPassword(rs.getString("password"));
-                member.setAge(rs.getInt("age"));
-                member.setAddr(rs.getString("addr"));
-                return member;
-            }
+        return super.executeQuery(sql, (RowMapper<Member>) (rs, rowNum) -> {
+            Member member = new Member();
+            member.setUsername(rs.getString("username"));
+            member.setPassword(rs.getString("password"));
+            member.setAge(rs.getInt("age"));
+            member.setAddr(rs.getString("addr"));
+            return member;
         }, null);
     }
 }
