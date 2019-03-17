73c884bf8e59373d0d575594bb32b0e12dc68320
panning255 <panning255@jk.cn>
2019-03-17 23:12:10 +0800

    add event observer pattern

---
+++ src/main/java/observer/events/MouseEventTest.java
@@ -0,0 +1,19 @@
+package observer.events;
+
+import observer.events.mouseevent.Mouse;
+import observer.events.mouseevent.MouseEventCallback;
+import observer.events.mouseevent.MouseEventType;
+
+public class MouseEventTest {
+    public static void main(String[] args) {
+        MouseEventCallback callback = new MouseEventCallback();
+        Mouse mouse = new Mouse();
+
+        //@谁？  @回调方法
+        mouse.addLisenter(MouseEventType.ON_CLICK,callback);
+        mouse.addLisenter(MouseEventType.ON_FOCUS,callback);
+
+        mouse.click();
+        mouse.focus();
+    }
+}
---
+++ src/main/java/observer/events/mouseevent/Keybord.java
@@ -0,0 +1,15 @@
+package observer.events.mouseevent;
+
+import observer.events.core.EventLisenter;
+
+public class Keybord extends EventLisenter {
+
+    public void down(){
+
+    }
+
+    public void up(){
+
+    }
+
+}
